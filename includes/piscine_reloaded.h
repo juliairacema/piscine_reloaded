9310f228b27af5c36e01490e4d91f034646bc49c
davhojt <davhojt@gmail.com>
2018-03-17 03:38:45 +0200

    Added mains for ft_is_negative and ft_ft

--- includes/piscine_reloaded.h
+++ includes/piscine_reloaded.h
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 02:07:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/03/17 02:52:56 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/03/17 03:35:43 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,15 @@ void	ft_putchar(char c);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr(char const *s);
 void	ft_putstr_fd(char const *s, int fd);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_itoa(int n);
+char	*ft_strnew(size_t size);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
 
 
 /*
@@ -32,5 +38,7 @@ size_t	ft_strlen(const char *str);
 */
 void	ft_print_alphabet(void);
 void	ft_print_numbers(void);
+void	ft_is_negative(int n);
+void	ft_ft(int *nbr);
 
 #endif
--- main.c
+++ main.c
@@ -20,6 +20,37 @@ void	ex07(void)
 	ft_putstr("\n\n");
 }
 
+void	ex08(void)
+{
+	ft_putstr("ex08\n");
+	ft_putstr("Expecting:\n\t");
+	ft_putstr("NNPPP\n");
+	ft_putstr("Result:\n\t");
+	ft_is_negative(-42);
+	ft_is_negative(-1);
+	ft_is_negative(0);
+	ft_is_negative(1);
+	ft_is_negative(42);
+	ft_putstr("\n\n");
+}
+
+void	ex09(void)
+{
+	int		i;
+	int		*nbr;
+
+	i = 9;
+	nbr = &i;
+
+	ft_putstr("ex09\n");
+	ft_putstr("Expecting:\n\t");
+	ft_putstr("42\n");
+	ft_putstr("Result:\n\t");
+	ft_ft(nbr);
+	ft_putnbr(*nbr);
+	ft_putstr("\n\n");
+}
+
 int		main(int argc, char **argv)
 {
 	if (argc == 2)
@@ -28,6 +59,10 @@ int		main(int argc, char **argv)
 			ex06();
 		if (ft_strcmp(argv[1], "ex07") == 0)
 			ex07();
+		if (ft_strcmp(argv[1], "ex08") == 0)
+			ex08();
+		if (ft_strcmp(argv[1], "ex09") == 0)
+			ex09();
 	}
 	return (0);
 }
