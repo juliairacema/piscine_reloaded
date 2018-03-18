7ee0203882cbb3d178ed6b3a0bcb7d20de9016fd
davhojt <davhojt@gmail.com>
2018-03-18 10:58:43 +0200

    Added ft_print_params

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/17 13:28:40 by dhojt             #+#    #+#             */
+/*   Updated: 2018/03/17 13:35:06 by dhojt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	
+	while (--argc)
+	{
+		ft_putstr(argv[++i]);
+		ft_putchar('\n');
+	}
+	return (0);
+}
