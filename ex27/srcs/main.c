b8b40c824e94489e98f081157d9f7f066d014020
davhojt <davhojt@gmail.com>
2018-03-19 22:10:09 +0200

    Added display_file excercise

---
+++ ex27/srcs/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/19 21:09:03 by dhojt             #+#    #+#             */
+/*   Updated: 2018/03/19 22:04:10 by dhojt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	str[1];
+
+	if (argc == 1)
+		write(1, "File name missing.\n", 19);
+	if (argc > 2)
+		write(1, "Too many arguments.\n", 20);
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd < 0)
+			return (0);
+		while (read(fd, str, 1))
+				write(1, &str, 1);
+		close(fd);
+		return (0);
+	}
+}
